732f8345fbc1cc9f1ffe17f89a395589cfb25ea7
Gareth Whitley <gareth@Gareths-MBP.lan>
2021-07-11 21:30:57 +0100

    removed comments text

--- jq.js
+++ jq.js
@@ -1,14 +1,6 @@
-/* 
-This basic module exports 
-a jQuery-like element ID and className selector
-to reduce the amount of unnecessary duplication
-in code when selecting many DOM elements.
-*/
-
 // Written by Gareth Whitley (c) 2021
 // www.github.com/gwhitdev
-// You may freely use and distribute this code as long
-// as you keep this attribution.
+// You may freely use and distribute this code as long as you keep this attribution.
 
 export function $(element) {
     if(element) {
